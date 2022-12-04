dcf76e1cceb325ccdd58744f9c2f6fab4fb67891
iliyason <71890027+iliyason@users.noreply.github.com>
2022-12-04 18:41:47 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    navigation: {
-        nextEl: ".right-s",
-        prevEl: ".left-s",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-});
+opening = false;
+elem = document.getElementById("menu");
+
+function MenuOpCl(){
+    if(opening == true){
+        elem.style.display="none";
+        opening = !opening;
+    }
+    else{
+        elem.style.display="flex";
+        opening = !opening;
+    }
+}
+
+el1 = document.getElementById("full-list-serv");
+el2 = document.getElementById("about");
+el3 = document.getElementById("contacts");
+function servise() {
+    el1.scrollIntoView({block: "start", behavior: "smooth"});
+}
+function about() {
+    el2.scrollIntoView({block: "start", behavior: "smooth"});
+}
+function aboutUs() {
+    el3.scrollIntoView({block: "start", behavior: "smooth"});
+}
